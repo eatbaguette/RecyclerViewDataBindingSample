36a4afe2a01766f87d76f5b68f4f620b0594e15a
gikeda <gikeda@appirits.com>
2018-03-06 13:25:17 +0900

    [add] Databindingを使ったRecyclerview 表示する

--- app/src/main/java/com/example/baguette/templatemvvm/views/MainActivity.kt
+++ app/src/main/java/com/example/baguette/templatemvvm/views/MainActivity.kt
@@ -3,15 +3,19 @@ package com.example.baguette.templatemvvm.views
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import com.example.baguette.templatemvvm.AppApplication
 import com.example.baguette.templatemvvm.R
 import com.example.baguette.templatemvvm.databinding.ActivityMainBinding
 import com.example.baguette.templatemvvm.viewmovel.MainViewModel
+import com.squareup.haha.perflib.Main
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var mainViewModel: MainViewModel
+
+    private lateinit var recyclerViewAdapter: MainRecyclerViewAdapter
 
     val binding: ActivityMainBinding by lazy {
         DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
@@ -24,5 +28,21 @@ class MainActivity : AppCompatActivity() {
         (application as AppApplication).appComponent.inject(this)
 
         binding.viewModel = mainViewModel
+
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        recyclerViewAdapter = MainRecyclerViewAdapter(this)
+        binding.mainRecyclerView.apply {
+            adapter = recyclerViewAdapter
+            layoutManager = LinearLayoutManager(this@MainActivity)
+        }
+
+        recyclerViewAdapter.upDateItems(
+                MainRecyclerViewAdapter.EvenNumberListItem(2),
+                MainRecyclerViewAdapter.OddNumberListItem(1)
+        )
+        recyclerViewAdapter.notifyDataSetChanged()
     }
 }
---
+++ app/src/main/java/com/example/baguette/templatemvvm/views/MainRecyclerViewAdapter.kt
@@ -0,0 +1,109 @@
+package com.example.baguette.templatemvvm.views
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.example.baguette.templatemvvm.R
+import com.example.baguette.templatemvvm.databinding.EvenNumberRecyclerViewCellBinding
+import com.example.baguette.templatemvvm.databinding.OddNumberRecyclerViewCellBinding
+import timber.log.Timber
+
+
+class MainRecyclerViewAdapter(private val context: Context): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private object ViewType {
+        const val EVEN_NUMBER = R.id.even_number
+        const val ODD_NUMBER = R.id.odd_number
+    }
+
+    private interface ViewItem {
+        val recyclerViewType: Int
+    }
+
+    private class EvenNumberViewItem(val item: EvenNumberListItem): ViewItem {
+        override val recyclerViewType: Int = ViewType.EVEN_NUMBER
+    }
+
+    private class OddNumberViewItem(val item: OddNumberListItem): ViewItem {
+        override val recyclerViewType: Int = ViewType.ODD_NUMBER
+    }
+
+    private class EvenNumberViewHolder(
+            val binding: EvenNumberRecyclerViewCellBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(viewItem: EvenNumberViewItem) {
+            binding.executePendingBindings()
+        }
+    }
+
+    private class OddNumberViewHolder(
+            val binding: OddNumberRecyclerViewCellBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(viewItem: OddNumberViewItem) {
+            binding.executePendingBindings()
+        }
+    }
+
+    private var items: List<ViewItem> = emptyList()
+
+    override fun getItemViewType(position: Int): Int {
+        return items[position].recyclerViewType
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder {
+        val inflater = LayoutInflater.from(context)
+
+        return when(viewType) {
+            ViewType.EVEN_NUMBER ->
+                    EvenNumberViewHolder(EvenNumberRecyclerViewCellBinding.inflate(inflater, parent, false))
+            ViewType.ODD_NUMBER ->
+                    OddNumberViewHolder(OddNumberRecyclerViewCellBinding.inflate(inflater, parent, false))
+            else ->
+                    throw RuntimeException("unknownViewType $viewType")
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return items.count()
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder?, position: Int) {
+        Timber.d("hoge view Type$holder")
+        when(holder) {
+            is EvenNumberViewHolder -> {
+                val item = items[position] as? EvenNumberViewItem ?: return
+                holder.bind(item)
+                Timber.d("hoge even")
+            }
+            is OddNumberViewHolder -> {
+                val item = items[position] as? OddNumberViewItem ?: return
+                Timber.d("hoge odd")
+                Timber.d("hoge odd $item")
+                holder.bind(item)
+            }
+        }
+    }
+
+    fun upDateItems(
+            evenNumberListItem: EvenNumberListItem,
+            oddNumberListItem: OddNumberListItem
+    ) {
+        val items = mutableListOf<ViewItem>()
+            items.add(EvenNumberViewItem(evenNumberListItem))
+            items.add(OddNumberViewItem(oddNumberListItem))
+
+        Timber.d("hoge item $items")
+
+        this.items = items
+        notifyDataSetChanged()
+    }
+
+
+    // とりあえずここにモデルを書いておく
+    data class OddNumberListItem(
+            private val number: Int
+    )
+
+    data class EvenNumberListItem(
+            private val number: Int
+    )
+}
